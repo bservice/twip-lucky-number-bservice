639f0f2ff3733b8af89a25ca65c40117bf9df3f2
bs666 <brservice@comcast.net>
2017-10-10 08:46:31 -0400

    I CAN ACTUALLY DO IT, YAAAAAAY

--- com/company/Main.java
+++ com/company/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         perfectRandom myLuckyNumber = new perfectRandom(2);
-        myLuckyNumber.square(2);
         Scanner kbInput = new Scanner(System.in);
         System.out.println("What is your name?");
         String name = kbInput.nextLine();
